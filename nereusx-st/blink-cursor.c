0eadf8fdec059836e5a1c457b4b4f778a530e39c
Nicholas Christopoulos <nereus@freemail.gr>
2019-12-04 00:42:22 +0200

    quick and dirty fix

--- nereusx-st/blink-cursor.c
+++ nereusx-st/blink-cursor.c
@@ -1,4 +1,8 @@
-// ndc: blinking cursor
+/*
+ *	blink cursor support
+ *	Copyleft (C) 2019 Nicholas Christopoulos
+ */
+
 #include <pthread.h>
 #include <unistd.h>
 static pthread_t t_curs;
@@ -7,18 +11,38 @@ static volatile int curs_blink_state= 0;
 static volatile int t_curs_init = 0;
 static volatile int t_curs_exit = 0;
 
+/*
+ *	timing
+ *	VT100 1/4 of sec (special mode)
+ *	CRT: min 20 (1/50Hz * 1000)
+ * 
+ *	\033[?12h = enable cursor blinking
+ *	\033[?12l = disable cursor blinking
+ * 
+ *	Linux 4.2+
+ *  \033[16;n] Set the cursor blink interval in milliseconds.
+ */
+static volatile int t_curs_vis_ms = 1250;
+static volatile int t_curs_inv_ms =  500;
+
+/*
+ *	change visible values
+ */
 void *tcurs_xchg(void *args)
 {
 	while ( !t_curs_exit ) {
 		curs_blink_state = !curs_blink_state;
 		if ( !curs_blink_state )
-			usleep(1250000);
+			usleep(t_curs_vis_ms * 1000);
 		else
-			usleep( 500000);
+			usleep(t_curs_inv_ms * 1000);
 		}
 	pthread_exit(NULL);
 }
 
+/*
+ *	returns 0 if cursor is no visible
+ */
 int	get_blink_value()
 {
 	if ( !t_blink_mode )
@@ -26,12 +50,27 @@ int	get_blink_value()
 	return curs_blink_state;	
 }
 
+/*
+ *	terminate (atexit)
+ */
 void tcurs_term()
 {
 	t_curs_exit = 1;
 	pthread_join(t_curs, NULL);
 	pthread_exit(NULL);
 }
+
+/*
+ *	enable/disable blinking
+ */
+void set_blink_cursor(int mode)
+{
+	t_blink_mode = mode;
+}
+
+/*
+ *	initialize mode
+ */
 void tcurs_init()
 {
 	if ( t_curs_init ==  0 ) {
@@ -41,7 +80,3 @@ void tcurs_init()
 	atexit(tcurs_term);
 }
 
-void set_blink_cursor(int mode)
-{
-	t_blink_mode = mode;
-}
