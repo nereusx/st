8ccc064226ae74587b1a83719b7be1394bf7f51d
Nicholas Christopoulos <nereus@freemail.gr>
2020-12-23 22:35:41 +0200

    S-Ins (paste) removed, only C-S-V

--- nereusx-st/config.def.h
+++ nereusx-st/config.def.h
@@ -106,7 +106,7 @@ static const char *colorname[] = {
 	"blue2",
 	"magenta3",
 	"cyan3",
-	"gray90",
+	"gray70",
 
 	/* 8 bright colors */
 	"gray50",
@@ -208,7 +208,7 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
+//	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
     { TERMMOD,              XK_Escape,      keyboard_select,{ 0 } },
 };
